e7e3f7e2f58b0b8c3e49f18bc0833fa1749d2c18
johnschatner <leontin2001@gmail.com>
2023-02-09 22:32:35 +0100

    Add hint

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -3,6 +3,7 @@ import "./Credit.css";
 export default function Credit() {
   return (
     <div className="credit noselect">
+      <div className="hint">hint: click clock</div>
       <span className="credit__by">Made by </span>
       <span className="credit__name">
         <a
